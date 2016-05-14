743785ace4f9911f88a52d3240e92db934f419a3
Yuhan Chen <yc195@users.noreply.github.com>
2016-05-14 01:32:51 -0400

    Update bdirmulttest.R

--- bdirmulttest.R
+++ bdirmulttest.R
@@ -3,7 +3,22 @@
 ##n1 and n2 are two groups of samples
 ##a and b are hyperparameters for the within-group variations eta1, eta2 \sim Ga(a,b)
 ##nq is the number of nodes for Gaussian-Laguerre quadrature
-bdirmulttest = function(n1,n2,a,b,nq)
+
+##==simple example--
+##S=8
+##K=4
+##n1=matrix(NA, nrow=S, ncol=K)
+##n2=matrix(NA, nrow=S, ncol=K)
+
+##base = rep(1/K,K)
+##  for (j in 1:S)
+##{
+##  n1[j,]=as.vector(rmultinom(1,ntest,rdirichlet(1,base*50)))
+##  n2[j,]=as.vector(rmultinom(1,ntest,rdirichlet(1,base*50)))
+##}
+require(statmod)
+
+bdirmulttest = function(n1,n2,a=10,b=0.1,nq=15)
 {
   K=dim(n1)[2]
   points=gauss.quad(nq,"laguerre",alpha=a-1)$nodes
